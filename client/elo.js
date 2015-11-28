530725f47f9e65790faa22dc0b7f1e311f6543d8
Brent Johnson <brent_johnson@abercrombie.com>
2015-11-28 09:33:34 -0500

    Restructuring

--- client/elo.js
+++ client/elo.js
@@ -1,3 +1,4 @@
+
 if (Meteor.isClient) {
   // counter starts at 0
   Session.setDefault('counter', 0);
@@ -19,5 +20,6 @@ if (Meteor.isClient) {
 if (Meteor.isServer) {
   Meteor.startup(function () {
     // code to run on server at startup
+//    ELO();
   });
 }
